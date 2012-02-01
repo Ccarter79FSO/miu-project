8a32721bd8a225198060c3bce9ecd182ecb84b32
Ccarter79fso <Ccarter79fso@fullsail.edu>
2012-02-01 04:05:43 -0500

    Added 17 more dummy data entries.

--- js/json.js
+++ js/json.js
@@ -16,7 +16,7 @@ var json = {
 		"notes"		:["Notes:", "Test1"]
 	},
 	"Game2": {
-		"gname"		:["Game Name:", "Game2"],
+		"gname"		:["Game Name:", "Game 2"],
 		"platform"	:["Platform:", "PC"],
 		"genre"		:["Genre:", ["RTS"]],
 		"fav"		:["Favorite:", "Yes"],
@@ -34,6 +34,186 @@ var json = {
 		"pdate"		:["Purchase Date:", "01-15-2012"],
 		"players"	:["Number of Players:", "1, or Single Player only"],
 		"notes"		:["Notes:", "Test3"]
+	},
+	"Game4": {
+		"gname"		:["Game Name:", "Game 4"],
+		"platform"	:["Platform:", "Xbox"],
+		"genre"		:["Genre:", ["FPS"]],
+		"fav"		:["Favorite:", "Yes"],
+		"rating"	:["Rating:", "5"],
+		"pdate"		:["Purchase Date:", "11-11-2011"],
+		"players"	:["Number of Players:", "2 players"],
+		"notes"		:["Notes:", "Test4"]
+	},
+	"Game5": {
+		"gname"		:["Game Name:", "Game 5"],
+		"platform"	:["Platform:", "Wii"],
+		"genre"		:["Genre:", ["Casual"]],
+		"fav"		:["Favorite:", "No"],
+		"rating"	:["Rating:", "4"],
+		"pdate"		:["Purchase Date:", "12-17-2011"],
+		"players"	:["Number of Players:", "4 or more Players"],
+		"notes"		:["Notes:", "Test5"]
+	},
+	"Game6": {
+		"gname"		:["Game Name:", "Game 6"],
+		"platform"	:["Platform:", "Other"],
+		"genre"		:["Genre:", ["Action/Adventure"]],
+		"fav"		:["Favorite:", "Yes"],
+		"rating"	:["Rating:", "10"],
+		"pdate"		:["Purchase Date:", "01-21-2012"],
+		"players"	:["Number of Players:", "1, or Single Player only"],
+		"notes"		:["Notes:", "Test6"]
+	},
+	"Game7": {
+		"gname"		:["Game Name:", "Game 7"],
+		"platform"	:["Platform:", "DS"],
+		"genre"		:["Genre:", ["FPS"]],
+		"fav"		:["Favorite:", "Yes"],
+		"rating"	:["Rating:", "5"],
+		"pdate"		:["Purchase Date:", "10-11-2011"],
+		"players"	:["Number of Players:", "1, or Single Player only"],
+		"notes"		:["Notes:", "Test7"]
+	},
+	"Game8": {
+		"gname"		:["Game Name:", "Game8"],
+		"platform"	:["Platform:", "PC"],
+		"genre"		:["Genre:", ["RTS"]],
+		"fav"		:["Favorite:", "Yes"],
+		"rating"	:["Rating:", "9"],
+		"pdate"		:["Purchase Date:", "09-13-2011"],
+		"players"	:["Number of Players:", "4 or more Players"],
+		"notes"		:["Notes:", "Test8"]
+	},
+	"Game9": {
+		"gname"		:["Game Name:", "Game 9"],
+		"platform"	:["Platform:", "PS3"],
+		"genre"		:["Genre:", ["RPG"]],
+		"fav"		:["Favorite:", "No"],
+		"rating"	:["Rating:", "1"],
+		"pdate"		:["Purchase Date:", "01-15-2011"],
+		"players"	:["Number of Players:", "1, or Single Player only"],
+		"notes"		:["Notes:", "Test9"]
+	},
+	"Game10": {
+		"gname"		:["Game Name:", "Game 10"],
+		"platform"	:["Platform:", "Xbox"],
+		"genre"		:["Genre:", ["FPS"]],
+		"fav"		:["Favorite:", "Yes"],
+		"rating"	:["Rating:", "5"],
+		"pdate"		:["Purchase Date:", "01-11-2012"],
+		"players"	:["Number of Players:", "4 or more Players"],
+		"notes"		:["Notes:", "Test10"]
+	},
+	"Game11": {
+		"gname"		:["Game Name:", "Game 11"],
+		"platform"	:["Platform:", "PC"],
+		"genre"		:["Genre:", ["RTS"]],
+		"fav"		:["Favorite:", "Yes"],
+		"rating"	:["Rating:", "9"],
+		"pdate"		:["Purchase Date:", "05-09-2011"],
+		"players"	:["Number of Players:", "4 or more Players"],
+		"notes"		:["Notes:", "Test11"]
+	},
+	"Game12": {
+		"gname"		:["Game Name:", "Game 12"],
+		"platform"	:["Platform:", "PS3"],
+		"genre"		:["Genre:", ["Sports"]],
+		"fav"		:["Favorite:", "Yes"],
+		"rating"	:["Rating:", "10"],
+		"pdate"		:["Purchase Date:", "09-15-2011"],
+		"players"	:["Number of Players:", "2 Players"],
+		"notes"		:["Notes:", "Test12"]
+	},
+	"Game13": {
+		"gname"		:["Game Name:", "Game 13"],
+		"platform"	:["Platform:", "Xbox"],
+		"genre"		:["Genre:", ["FPS"]],
+		"fav"		:["Favorite:", "Yes"],
+		"rating"	:["Rating:", "5"],
+		"pdate"		:["Purchase Date:", "04-17-2011"],
+		"players"	:["Number of Players:", "4 or more Players"],
+		"notes"		:["Notes:", "Test13"]
+	},
+	"Game14": {
+		"gname"		:["Game Name:", "Game 14"],
+		"platform"	:["Platform:", "PC"],
+		"genre"		:["Genre:", ["RPG"]],
+		"fav"		:["Favorite:", "Yes"],
+		"rating"	:["Rating:", "9"],
+		"pdate"		:["Purchase Date:", "11-17-2011"],
+		"players"	:["Number of Players:", "1, or Single Player only"],
+		"notes"		:["Notes:", "Test14"]
+	},
+	"Game15": {
+		"gname"		:["Game Name:", "Game 15"],
+		"platform"	:["Platform:", "Wii"],
+		"genre"		:["Genre:", ["Simulation"]],
+		"fav"		:["Favorite:", "No"],
+		"rating"	:["Rating:", "2"],
+		"pdate"		:["Purchase Date:", "01-01-2012"],
+		"players"	:["Number of Players:", "1, or Single Player only"],
+		"notes"		:["Notes:", "Test15"]
+	},
+	"Game16": {
+		"gname"		:["Game Name:", "Game 16"],
+		"platform"	:["Platform:", "PC"],
+		"genre"		:["Genre:", ["FPS", "RPG"]],
+		"fav"		:["Favorite:", "Yes"],
+		"rating"	:["Rating:", "10"],
+		"pdate"		:["Purchase Date:", "12-11-2011"],
+		"players"	:["Number of Players:", "4 or more Players"],
+		"notes"		:["Notes:", "Test16"]
+	},
+	"Game17": {
+		"gname"		:["Game Name:", "Game 17"],
+		"platform"	:["Platform:", "PC"],
+		"genre"		:["Genre:", ["FPS"]],
+		"fav"		:["Favorite:", "No"],
+		"rating"	:["Rating:", "4"],
+		"pdate"		:["Purchase Date:", "06-05-2011"],
+		"players"	:["Number of Players:", "4 or more Players"],
+		"notes"		:["Notes:", "Test17"]
+	},
+	"Game18": {
+		"gname"		:["Game Name:", "Game 18"],
+		"platform"	:["Platform:", "PS3"],
+		"genre"		:["Genre:", ["RPG", "Action/Adventure"]],
+		"fav"		:["Favorite:", "No"],
+		"rating"	:["Rating:", "1"],
+		"pdate"		:["Purchase Date:", "01-15-2012"],
+		"players"	:["Number of Players:", "1, or Single Player only"],
+		"notes"		:["Notes:", "Test18"]
+	},
+	"Game19": {
+		"gname"		:["Game Name:", "Game 19"],
+		"platform"	:["Platform:", "Xbox"],
+		"genre"		:["Genre:", ["FPS"]],
+		"fav"		:["Favorite:", "Yes"],
+		"rating"	:["Rating:", "5"],
+		"pdate"		:["Purchase Date:", "07-11-2011"],
+		"players"	:["Number of Players:", "4 or more Players"],
+		"notes"		:["Notes:", "Test19"]
+	},
+	"Game20": {
+		"gname"		:["Game Name:", "Game 20"],
+		"platform"	:["Platform:", "PC"],
+		"genre"		:["Genre:", ["Simulation"]],
+		"fav"		:["Favorite:", "Yes"],
+		"rating"	:["Rating:", "9"],
+		"pdate"		:["Purchase Date:", "12-17-2011"],
+		"players"	:["Number of Players:", "4 or more Players"],
+		"notes"		:["Notes:", "Test20"]
+	},
+	"Game21": {
+		"gname"		:["Game Name:", "Game 21"],
+		"platform"	:["Platform:", "PS3"],
+		"genre"		:["Genre:", ["Action/Adventure"]],
+		"fav"		:["Favorite:", "No"],
+		"rating"	:["Rating:", "6"],
+		"pdate"		:["Purchase Date:", "10-15-2011"],
+		"players"	:["Number of Players:", "2 Player"],
+		"notes"		:["Notes:", "Test21"]
 	}
 
 }
